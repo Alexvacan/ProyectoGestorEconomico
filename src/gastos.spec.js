da11f058c37aebf7c27355ba2f676ad7f6774743
Alexander <alexander.vacaflores@ucb.edu.bo>
2024-10-24 02:22:16 -0400

    Implementación inicial de la clase Gastos

--- src/gastos.spec.js
+++ src/gastos.spec.js
@@ -1,60 +1,18 @@
-import Gastos from "./gastos.js";
+class Gastos {
+  constructor() {
+    this.gastos = [];
+  }
 
-describe("Gastos", () => {
-  it("registrar un gasto", () => {
-    //Given -- arrange
-    const gastos = new Gastos();
-    const registroGasto = {
-      fecha: "2024-10-12",
-      monto: 45,
-      descripcion: "compra de libros",
-    };
+  registrarGasto(gasto) {
+    if (!gasto.fecha || !gasto.monto || !gasto.descripcion) {
+      throw new Error("Porfavor llene todos los campos");
+    }
+    this.gastos.push(gasto);
+  }
 
-    //When -act
-    gastos.registrarGasto(registroGasto);
+  obtenerGastos() {
+    return this.gastos;
+  }
+}
 
-    //Then --assert
-    let gastoRegistrado = gastos.obtenerGastos();
-    expect(gastoRegistrado).toEqual([registroGasto]);
-  });
-
-  it("registrar un gasto en pasajes", () => {
-    //Given -- arrange
-    const gastos = new Gastos();
-    const registroGasto = {
-      fecha: "2024-08-12",
-      monto: 20,
-      descripcion: "pasajes",
-    };
-
-    //When -act
-    gastos.registrarGasto(registroGasto);
-
-    //Then --assert
-    let gastoRegistrado = gastos.obtenerGastos();
-    expect(gastoRegistrado).toEqual([registroGasto]);
-  });
-
-  it("registrar varios gastos", () => {
-    //Given -- arrange
-    const gastos = new Gastos();
-    const  gastoPasajes = {
-      fecha: "2024-08-12",
-      monto: 20,
-      descripcion: "pasajes",
-    };
-    gastos.registrarGasto(gastoPasajes);
-
-    //When -act
-    const gastoCine = {
-      fecha: "2024-05-06",
-      monto: 23,
-      descripcion: "cine",
-    };
-    gastos.registrarGasto(gastoCine);
-
-    //Then --assert
-    let gastosRegistrados = gastos.obtenerGastos();
-    expect(gastosRegistrados).toEqual([gastoPasajes, gastoCine]);
-  });
-});
+export default Gastos;
--- src/ingresos.spec.js
+++ src/ingresos.spec.js
@@ -4,7 +4,7 @@ describe("Ingresos", () => {
   it("debería registrar un ingreso correctamente", () => {
     const ingresos = new Ingresos();
     const ingresoValido = {
-      fecha: "2024-10-15", monto: 1000, descripcion: "salario",
+      fecha: "2024-10-15", monto: 1853, descripcion: "salario",
     };
 
     ingresos.registrarIngreso(ingresoValido);
