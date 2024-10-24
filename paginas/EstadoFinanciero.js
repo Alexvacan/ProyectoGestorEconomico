16b8f1399eecc609d46728143697ad7225ddb2ab
Alexander <alexander.vacaflores@ucb.edu.bo>
2024-10-24 06:13:24 -0400

    Ultima prueba

---
+++ paginas/EstadoFinanciero.js
@@ -0,0 +1,35 @@
+class EstadoFinanciero {
+  constructor() {
+    this.ingresos = [];
+    this.gastos = [];
+    this.presupuesto = 0; 
+  }
+
+  registrarIngreso(ingreso) {
+    if (!ingreso.fecha || !ingreso.monto || !ingreso.descripcion) {
+      throw new Error("Por favor llene todos los campos");
+    }
+    this.ingresos.push(ingreso);
+  }
+
+  registrarGasto(gasto) {
+    if (!gasto.fecha || !gasto.monto || !gasto.descripcion) {
+      throw new Error("Por favor llene todos los campos");
+    }
+    this.gastos.push(gasto);
+  }
+
+  calcularPorcentajeGastado() {
+    const totalIngresos = this.ingresos.reduce((total, ingreso) => total + ingreso.monto, 0);
+    const totalGastos = this.gastos.reduce((total, gasto) => total + gasto.monto, 0);
+    return totalIngresos > 0 ? (totalGastos / totalIngresos) * 100 : 0; 
+  }
+
+  descontarDelPresupuesto(monto) {
+    if (monto > this.presupuesto) {
+      throw new Error("No se puede descontar más del presupuesto disponible");
+    }
+    this.presupuesto -= monto; 
+  }
+}
+module.exports = EstadoFinanciero;
