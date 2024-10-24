8404c67966f513a5c81fdb9bbaf694ae05495578
Alexander <alexander.vacaflores@ucb.edu.bo>
2024-10-24 02:14:55 -0400

    Agregar prueba para registrarIngreso en la clase Ingresos (prueba fallida) nuevamente

--- src/ingresos.spec.js
+++ src/ingresos.spec.js
@@ -1,12 +1,11 @@
 import Ingresos from "./ingresos";
 
 describe("Ingresos", () => {
-  it("debería registrar un ingreso correctamente", () => {
-    const ingresos = new Ingresos();
-    const ingreso = { fecha: "2024-10-15", monto: 1000, descripcion: "salario" };
-
-    ingresos.registrarIngreso(ingreso);
-
-    expect(ingresos.obtenerIngresos()).toEqual([ingreso]);
-  });
+    it("debería lanzar un error si falta un campo", () => {
+        const ingresos = new Ingresos();
+        const ingresoInvalido = { }; // Solo monto
+      
+        expect(() => ingresos.registrarIngreso(ingresoInvalido)).toThrowError(new Error("Porfavor llene todos los campos"));
+      });
 });
+
