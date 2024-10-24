41dc95976cf3df2cc35b1fe9fe9b62e2747a6ee8
Jhessael Siancas <siancas.jhessael@gmail.com>
2024-10-24 01:11:43 -0400

    funcion cambiada a clase usuario

--- src/1Login/optener_datos.js
+++ src/1Login/optener_datos.js
@@ -1,6 +1,20 @@
-function datos() {
+class Ususarios {
+  constructor() {
+    this.nombre = [];
+    this.contrasenia = [];
+  }
+  registrarNombre(nombre) {
+    this.nombre.push(nombre);
+  }
+  obtenerNombre() {
+    return this.nombre;
+  }
 
-    
+  registrarContrasenia(contrasenia) {
+    this.contrasenia.push(contrasenia);
+  }
+  obtenerContrasenia() {
+    return this.contrasenia;
+  }
 }
-
-export default datos;
+export default Ususarios;
