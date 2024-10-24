036c78314e18d8610824149bbb999adbe74c09c0
Alexander <alexander.vacaflores@ucb.edu.bo>
2024-10-24 03:31:11 -0400

    Modificacion y ahora todas las pruebas pasan

--- src/ingresos.js
+++ src/ingresos.js
@@ -1,22 +1,21 @@
 class Ingresos {
     constructor() {
-      this.ingresos = [];
+        this.ingresos = [];
     }
-  
+
     registrarIngreso(ingreso) {
-      if (!ingreso.fecha || !ingreso.monto || !ingreso.descripcion) {
-        throw new Error("Porfavor llene todos los campos");
-      }
-      if (ingreso.monto <= 0) {
-        throw new Error("El monto debe ser mayor a 0");
-      }
-      this.ingresos.push(ingreso);
+        if (!ingreso.fecha || !ingreso.monto || !ingreso.descripcion) {
+            throw new Error("Porfavor llene todos los campos");
+        }
+        if (ingreso.monto <= 0) { // Validación para montos negativos
+            throw new Error("El monto debe ser mayor que 0");
+        }
+        this.ingresos.push(ingreso);
     }
-  
+
     obtenerIngresos() {
-      return this.ingresos;
+        return this.ingresos;
     }
-  }
-  
-  export default Ingresos;
-  
+}
+
+export default Ingresos;
--- src/ingresos.spec.js
+++ src/ingresos.spec.js
@@ -5,16 +5,12 @@ describe("Ingresos", () => {
         const ingresos = new Ingresos();
         
         const ingresoSalario = {
-          fecha: "2024-10-15", 
-          monto: 1850,
-          descripcion: "salario",
+          fecha: "2024-10-15", monto: 1850, descripcion: "salario",
         };
         ingresos.registrarIngreso(ingresoSalario);
         
         const ingresoBono = {
-          fecha: "2024-10-20",
-          monto: 500,
-          descripcion: "bono de desempeño",
+          fecha: "2024-10-20", monto: 500, descripcion: "bono de desempeño",
         };
         
         ingresos.registrarIngreso(ingresoBono);
@@ -23,14 +19,16 @@ describe("Ingresos", () => {
         expect(ingresosRegistrados).toEqual([ingresoSalario, ingresoBono]);
     });
 
+
     it("debería lanzar un error si el monto es negativo", () => {
         const ingresos = new Ingresos();
-        const ingresoInvalido = {
-            fecha: "2024-10-25",
-            monto: -200,
+        const ingresoInvalido = { 
+            fecha: "2024-10-25", 
+            monto: -200, 
             descripcion: "pérdida de inversión",
         };
-
+    
         expect(() => ingresos.registrarIngreso(ingresoInvalido)).toThrow("El monto debe ser mayor que 0");
     });
+    
 });
