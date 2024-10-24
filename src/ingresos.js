653d02f027be02ed0d006258220e36f35be105f1
Alexander <alexander.vacaflores@ucb.edu.bo>
2024-10-24 03:21:29 -0400

    Nueva prueba fallida, el monto es negativo por lo tanto falla

--- src/ingresos.js
+++ src/ingresos.js
@@ -7,6 +7,9 @@ class Ingresos {
       if (!ingreso.fecha || !ingreso.monto || !ingreso.descripcion) {
         throw new Error("Porfavor llene todos los campos");
       }
+      if (ingreso.monto <= 0) {
+        throw new Error("El monto debe ser mayor a 0");
+      }
       this.ingresos.push(ingreso);
     }
   
--- src/ingresos.spec.js
+++ src/ingresos.spec.js
@@ -5,7 +5,8 @@ describe("Ingresos", () => {
         const ingresos = new Ingresos();
         
         const ingresoSalario = {
-          fecha: "2024-10-15", monto: 1850,
+          fecha: "2024-10-15", 
+          monto: 1850,
           descripcion: "salario",
         };
         ingresos.registrarIngreso(ingresoSalario);
@@ -15,10 +16,21 @@ describe("Ingresos", () => {
           monto: 500,
           descripcion: "bono de desempeño",
         };
+        
         ingresos.registrarIngreso(ingresoBono);
       
         const ingresosRegistrados = ingresos.obtenerIngresos();
         expect(ingresosRegistrados).toEqual([ingresoSalario, ingresoBono]);
-      });
-      
+    });
+
+    it("debería lanzar un error si el monto es negativo", () => {
+        const ingresos = new Ingresos();
+        const ingresoInvalido = {
+            fecha: "2024-10-25",
+            monto: -200,
+            descripcion: "pérdida de inversión",
+        };
+
+        expect(() => ingresos.registrarIngreso(ingresoInvalido)).toThrow("El monto debe ser mayor que 0");
+    });
 });
