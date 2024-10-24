e2bdd9fbd8eacb45b2418ffae2b656f9c32ed018
Tairin Muñoz <tairinmunoz14@gmail.com>
2024-10-24 04:02:20 -0400

    Se agrego la funcion de ahorro

---
+++ src/ahorros.js
@@ -0,0 +1,18 @@
+class Ahorros {
+    constructor() {
+      this.ahorros = [];
+    }
+  
+    registrarAhorro(ahorro) 
+    {
+      // Esto no hace nada
+    }
+  
+    obtenerAhorros() {
+      // Devolvemos siempre una lista vacía para asegurarnos que falle
+      return ;
+    }
+  }
+  
+  export default Ahorros;
+  
