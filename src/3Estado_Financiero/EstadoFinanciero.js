935d2ec0fb799acc81792fa00e3cd3caa33e2608
AngelDRS <davidrios1313ma@gmail.com>
2024-10-24 04:40:09 -0300

    Se creo la pagina de ingresos y gastos, con las funcionalidades

--- src/3Estado_Financiero/EstadoFinanciero.js
+++ src/3Estado_Financiero/EstadoFinanciero.js
@@ -1,35 +1,38 @@
 class EstadoFinanciero {
     constructor() {
-      this.ingresos = [];
-      this.gastos = [];
-      this.presupuesto = 0; 
+        this.ingresos = [];
+        this.gastos = [];
+        this.presupuesto = 0; 
     }
-  
+
     registrarIngreso(ingreso) {
-      if (!ingreso.fecha || !ingreso.monto || !ingreso.descripcion) {
-        throw new Error("Por favor llene todos los campos");
-      }
-      this.ingresos.push(ingreso);
+        if (!ingreso.fecha || !ingreso.monto || !ingreso.descripcion) {
+            throw new Error("Por favor llene todos los campos");
+        }
+        this.ingresos.push(ingreso);
+        this.presupuesto += ingreso.monto; 
     }
-  
+
     registrarGasto(gasto) {
-      if (!gasto.fecha || !gasto.monto || !gasto.descripcion) {
-        throw new Error("Por favor llene todos los campos");
-      }
-      this.gastos.push(gasto);
+        if (!gasto.fecha || !gasto.monto || !gasto.descripcion) {
+            throw new Error("Por favor llene todos los campos");
+        }
+        this.gastos.push(gasto);
+        this.presupuesto -= gasto.monto; 
     }
-  
+
     calcularPorcentajeGastado() {
-      const totalIngresos = this.ingresos.reduce((total, ingreso) => total + ingreso.monto, 0);
-      const totalGastos = this.gastos.reduce((total, gasto) => total + gasto.monto, 0);
-      return totalIngresos > 0 ? (totalGastos / totalIngresos) * 100 : 0; 
+        const totalIngresos = this.ingresos.reduce((total, ingreso) => total + ingreso.monto, 0);
+        const totalGastos = this.gastos.reduce((total, gasto) => total + gasto.monto, 0);
+        return totalIngresos > 0 ? (totalGastos / totalIngresos) * 100 : 0; 
     }
-  
+
     descontarDelPresupuesto(monto) {
-      if (monto > this.presupuesto) {
-        throw new Error("No se puede descontar más del presupuesto disponible");
-      }
-      this.presupuesto -= monto; 
+        if (monto > this.presupuesto) {
+            throw new Error("No se puede descontar más del presupuesto disponible");
+        }
+        this.presupuesto -= monto; 
     }
-  }
-  module.exports = EstadoFinanciero;
+}
+
+module.exports = EstadoFinanciero;
