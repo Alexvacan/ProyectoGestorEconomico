c61b6d6472f908af2727d3e3f48c31314fdcca99
Jhessael Siancas <siancas.jhessael@gmail.com>
2024-11-14 08:41:56 -0400

    Las funciones de Guardar y eliminar datos funciona correctamente

--- paginas/IngresoDatosDiarios/GuardarEliminarDatos.js
+++ paginas/IngresoDatosDiarios/GuardarEliminarDatos.js
@@ -16,10 +16,6 @@ document.getElementById('form-datos').addEventListener('submit',function(e){
         localStorage.setItem(NombreClave,['']);
     }
     
-    
-    //console.log('==============INGRESAR==============')
-    
-    
     let dato =localStorage.getItem(NombreClave)
     dato=dato.split(',')
     if(dato[0]==='')
@@ -29,25 +25,16 @@ document.getElementById('form-datos').addEventListener('submit',function(e){
     console.log(dato)
     dato.push(document.getElementById('dato').value);
     console.log(dato)
-    //Guarda el dato en el local Storage
-    //la sentencia es: localStorage.setItem('calve',valor)
     localStorage.setItem(NombreClave,dato);
-    //Corfirmacion al usuario
-    //alert('Dato guardado exitosamente')
-    console.log("datos:",dato)
+    
 })
 document.getElementById('form-eliminar').addEventListener('submit',function(e){
     e.preventDefault();
-    console.log('-----------------ELIMINAR--------------')
-    let Numero_a_eliminar=document.getElementById('dato-a-eliminar').value
+    let Numero_a_eliminar=Number(document.getElementById('dato-a-eliminar').value)
     let dato=localStorage.getItem(NombreClave)
-    let posNumeroAEliminar=dato.indexOf(Numero_a_eliminar.toString());
-    posNumeroAEliminar=posNumeroAEliminar/2
     dato=dato.split(',')
-/*  console.log("dato a eliminar:",Numero_a_eliminar)
-    console.log('Datos: ',dato)
-    console.log('tipo:',typeof(dato[0]))
-    console.log('pocicion dato a eliminar:',posNumeroAEliminar)*/
+    dato=convertirArrayAEnteros(dato);
+    let posNumeroAEliminar=dato.indexOf(Numero_a_eliminar);
     if(posNumeroAEliminar>=0)
     {
         dato.splice(posNumeroAEliminar,1)
@@ -56,7 +43,5 @@ document.getElementById('form-eliminar').addEventListener('submit',function(e){
     {
         alert("No existe el numero que quiere eliminar")
     }
-    //console.log('Datos Despues de eliminar: ',dato)
-    
     localStorage.setItem(NombreClave,dato)
 })
