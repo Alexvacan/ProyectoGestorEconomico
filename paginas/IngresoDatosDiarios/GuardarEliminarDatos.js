d83b2cd0e753f4272bd6a7460205a3bf496be0e7
Jhessael Siancas <siancas.jhessael@gmail.com>
2024-11-15 21:43:59 -0400

    funcion de GuardarEliminarDatos refactorizada y ahora guarda datos de la fecha actual

--- paginas/IngresoDatosDiarios/GuardarEliminarDatos.js
+++ paginas/IngresoDatosDiarios/GuardarEliminarDatos.js
@@ -1,49 +1,83 @@
-const NombreClave='datos-De-Gastos-diarios'
+const NombreClave = "datos-De-Gastos-diarios";
+const NombreClaveFechas="Fecha"
+const NombreClaveHoras="Hora"
 
-function convertirArrayAEnteros(array)
+function ControlarQueLocaStoreNoEsteVacio(nombreClave)
 {
-    for(let i=0;i<array.length;i++)
-    {
-        array[i]=Number(array[i])
-    }
-    return array;
+  if (localStorage.getItem(nombreClave) === null) {
+    localStorage.setItem(nombreClave, [""]);
+  }
 }
 
-document.getElementById('form-datos').addEventListener('submit',function(e){
-    e.preventDefault();
-    if(localStorage.getItem(NombreClave)===null)
-    {
-        localStorage.setItem(NombreClave,['']);
-    }
-    
-    let DatosGastos =localStorage.getItem(NombreClave)
-    DatosGastos=DatosGastos.split(',')
-    if(DatosGastos[0]==='')
-    {
-        DatosGastos.shift();
-    }
-    console.log(DatosGastos)
-    DatosGastos.push(document.getElementById('dato').value);
-    console.log(DatosGastos)
-    localStorage.setItem(NombreClave,DatosGastos);
-    
-})
+function EliminarDatoInicialSiEstaVacio(array)
+{
+  if (array[0] === "") {
+    array.shift();
+  }
+  return array;
+}
 
 
-document.getElementById('form-eliminar').addEventListener('submit',function(e){
+function OptenerDatosLocalStoreCorrectamente(nombreClave)
+{
+  ControlarQueLocaStoreNoEsteVacio(nombreClave);
+  let datosOptenidos=localStorage.getItem(nombreClave)
+  datosOptenidos=datosOptenidos.split(",")
+
+  datosOptenidos=EliminarDatoInicialSiEstaVacio(datosOptenidos);
+
+  return datosOptenidos;
+}
+
+
+function convertirArrayAEnteros(array) {
+  for (let i = 0; i < array.length; i++) {
+    array[i] = Number(array[i]);
+  }
+  return array;
+}
+
+document.getElementById("form-datos").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const fechaActual = new Date();
+  let dia = fechaActual.getDate();
+  let mes = fechaActual.getMonth() + 1;
+  let anio = fechaActual.getFullYear();
+  let hora = fechaActual.getHours();
+  let minuto = fechaActual.getMinutes();
+
+  let fechaMesDiaActual = anio.toString() + "-" + mes.toString() + "-" + dia.toString();
+  let horaActual = hora.toString() + ":" + minuto.toString();
+
+  let fechasGuardadas=OptenerDatosLocalStoreCorrectamente(NombreClaveFechas)
+  let horasGuardadas=OptenerDatosLocalStoreCorrectamente(NombreClaveHoras)
+
+
+  let DatosGastos = OptenerDatosLocalStoreCorrectamente(NombreClave);
+  console.log(DatosGastos)
+  DatosGastos.push(document.getElementById("dato").value);
+  fechasGuardadas.push(fechaMesDiaActual)
+  horasGuardadas.push(horaActual)
+
+  localStorage.setItem(NombreClave, DatosGastos);
+  localStorage.setItem(NombreClaveFechas,fechasGuardadas);
+  localStorage.setItem(NombreClaveHoras, horasGuardadas);
+
+});
+
+document.getElementById("form-eliminar").addEventListener("submit", function (e) {
     e.preventDefault();
-    let Numero_a_eliminar=Number(document.getElementById('dato-a-eliminar').value)
-    let DatosGastos=localStorage.getItem(NombreClave)
-    DatosGastos=DatosGastos.split(',')
-    DatosGastos=convertirArrayAEnteros(DatosGastos);
-    let posNumeroAEliminar=DatosGastos.indexOf(Numero_a_eliminar);
-    if(posNumeroAEliminar>=0)
-    {
-        DatosGastos.splice(posNumeroAEliminar,1)
+    let Numero_a_eliminar = Number(
+      document.getElementById("dato-a-eliminar").value
+    );
+
+    let DatosGastos = OptenerDatosLocalStoreCorrectamente(NombreClave);
+    DatosGastos = convertirArrayAEnteros(DatosGastos);
+    let posNumeroAEliminar = DatosGastos.indexOf(Numero_a_eliminar);
+    if (posNumeroAEliminar >= 0) {
+      DatosGastos.splice(posNumeroAEliminar, 1);
+    } else {
+      alert("No existe el numero que quiere eliminar");
     }
-    else
-    {
-        alert("No existe el numero que quiere eliminar")
-    }
-    localStorage.setItem(NombreClave,DatosGastos)
-})
+    localStorage.setItem(NombreClave, DatosGastos);
+  });
