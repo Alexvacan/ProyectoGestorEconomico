d11d0fc229cd881707bab28957e3bfd8c708913a
Jhessael Siancas <siancas.jhessael@gmail.com>
2024-11-28 09:09:35 -0400

    Segunda prueba TDD del Manejor de datos EliminarDatoInicial falla

--- paginas/IngresoDatosDiarios/ManejoDeDatos.spec.js
+++ paginas/IngresoDatosDiarios/ManejoDeDatos.spec.js
@@ -4,6 +4,10 @@ describe("Manejo de arrays",()=>{
     it("La funcion deberia devolver la funcion tal cual si el valor en la pocicion 0 no esta vacio",()=>{
         let array=['1','2','3']
         expect(manejoDeDatos.EliminarDatoInicialSiEstaVacio(array)).toEqual(array)
-
+    })
+    it("La funcion deberia devolver la funcion eliminando la pocicion 0 si esta esta vacia",()=>{
+        let array=['','2','3']
+        let arrayEsperado=['2','3']
+        expect(manejoDeDatos.EliminarDatoInicialSiEstaVacio(array)).toEqual(arrayEsperado)
     })
 })
