4c9d387fab919cdae2443db02bc0468f139d73b3
Jhessael Siancas <siancas.jhessael@gmail.com>
2024-12-01 14:22:44 -0400

    Refactoring: Se creo la funcion mostrar datos para evitar codigo repetido

--- paginas/MostrarDatos/mostrar.js
+++ paginas/MostrarDatos/mostrar.js
@@ -10,29 +10,35 @@ const NombreClaveIngresosFechas="Fecha-Ingresos"
 const NombreClaveIngresosHoras="Hora-Ingresos"
 
 
-let DatosGastos=localStorage.getItem(NombreClave)
+let DatosGastos=manejoDeDatos.OptenerDatosLocalStoreCorrectamente(NombreClave)
 let fechasGuardadas=localStorage.getItem(NombreClaveFechas)
 let horasGuardadas=localStorage.getItem(NombreClaveHoras)
 
 
 let mostrarDiv=document.getElementById('mostrar-Gastos')
-DatosGastos=DatosGastos.split(',')
 fechasGuardadas=fechasGuardadas.split(',')
 horasGuardadas=horasGuardadas.split(',')
 
-if(DatosGastos.length>=0){
-    for(let i=0;i<DatosGastos.length;i++)
-    {
-        let divElemento = document.createElement('div');
-        divElemento.classList.add('Gasto')
-        divElemento.textContent="Fecha del Gasto: "+fechasGuardadas[i]+" Hora: "+horasGuardadas[i]+" Monto: "+DatosGastos[i];
-        mostrarDiv.appendChild(divElemento);
-    }
 
+
+function MostrarDatos(datos,idElemento)
+{
+    if(datos.length>=0){
+        for(let i=0;i<datos.length;i++)
+        {
+            let divElemento = document.createElement('div');
+            divElemento.classList.add(idElemento)
+            divElemento.textContent="Fecha del Gasto: "+fechasGuardadas[i]+" Hora: "+horasGuardadas[i]+" Monto: "+DatosGastos[i];
+            mostrarDiv.appendChild(divElemento);
+        }
+
+    }
+    else{
+        mostrarDiv.textContent='No hay datos guardados.'
+    }
 }
-else{
-    mostrarDiv.textContent='No hay datos guardados.'
-}
+
+MostrarDatos(DatosGastos,'Gasto')
 
 let DatosIngresos=localStorage.getItem(NombreClaveIngresos)
 let fechasGuardadasIngresos=localStorage.getItem(NombreClaveIngresosFechas)
