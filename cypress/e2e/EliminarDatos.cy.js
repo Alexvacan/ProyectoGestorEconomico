c7e442743d36751a5ed3a0c79aacc56b63da3c90
Jhessael Siancas <siancas.jhessael@gmail.com>
2024-12-02 11:24:57 -0400

    segunda prueba cypress funcionando

--- cypress/e2e/EliminarDatos.cy.js
+++ cypress/e2e/EliminarDatos.cy.js
@@ -1,9 +1,9 @@
-describe('Prueba de botón "Ingresar Ingresos"', () => {
+describe('Prueba de botón "Eliminar Gastos"', () => {
     beforeEach(() => {
       cy.clearLocalStorage();
     });
   
-    it("Verifica que se pueda hacer insertar el valor de 34", () => {  
+    it("Verifica que se pueda hacer insertar y eliminar el valor de 34", () => {  
       cy.visit("/paginas/IngresoDatosDiarios/PaginaPrincipal.html");
       cy.get('#dato').type(34)
       cy.get('#form-datos-Gastos').within(() => {
@@ -17,4 +17,26 @@ describe('Prueba de botón "Ingresar Ingresos"', () => {
       cy.contains("Ingrese aqui para ver los Datos").click();
       cy.url().should("include", "/MostrarDatos/mostrar.html");
     });
+
+    it("Verifica que se pueda hacer insertar y eliminar el valor de 12", () => {
+        cy.visit("/paginas/IngresoDatosDiarios/PaginaPrincipal.html");
+      cy.get('#dato').type(12)
+      cy.get('#form-datos-Gastos').within(() => {
+          cy.get('button').click();
+        });
+
+      cy.get("#dato-a-eliminar").type(12);
+      cy.get("#form-eliminar-Gastos").within(() => {
+        cy.get("button").click();
+      });
+      cy.contains("Ingrese aqui para ver los Datos").click();
+      cy.url().should("include", "/MostrarDatos/mostrar.html");
+      });
+
+
 });
+
+
+
+
+
