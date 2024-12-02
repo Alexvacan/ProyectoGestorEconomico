3d1bbd080ba95be5a337d1d8b02ddd62dc8d39ed
Jhessael Siancas <siancas.jhessael@gmail.com>
2024-12-02 12:03:15 -0400

    Se agrego la prueba cypress para la seccion para eliminar los datos

--- cypress/e2e/EliminarDatos.cy.js
+++ cypress/e2e/EliminarDatos.cy.js
@@ -76,10 +76,92 @@ describe('Prueba de botón "Eliminar Gastos"', () => {
             cy.contains("Ingrese aqui para ver los Datos").click();
             cy.url().should("include", "/MostrarDatos/mostrar.html");
         });
-
 });
 
 
 
 
 
+
+
+
+
+describe('Prueba de botón "Eliminar Ingresos"', () => {
+    beforeEach(() => {
+      cy.clearLocalStorage();
+    });
+  
+    it("Verifica que se pueda hacer insertar y eliminar el valor de 34", () => {  
+      cy.visit("/paginas/IngresoDatosDiarios/PaginaPrincipal.html");
+      cy.get('#dato-Ingresos').type(34)
+        cy.get('#form-datos-Ingresos').within(() => {
+            cy.get('button').click();
+          });
+
+      cy.get("#dato-a-eliminar-Ingresos").type(34);
+      cy.get("#form-eliminar-Ingresos").within(() => {
+        cy.get("button").click();
+      });
+      cy.contains("Ingrese aqui para ver los Datos").click();
+      cy.url().should("include", "/MostrarDatos/mostrar.html");
+    });
+
+    it("Verifica que se pueda hacer insertar y eliminar el valor de 12", () => {
+        cy.visit("/paginas/IngresoDatosDiarios/PaginaPrincipal.html");
+        cy.get('#dato-Ingresos').type(12)
+        cy.get('#form-datos-Ingresos').within(() => {
+            cy.get('button').click();
+          });
+
+        cy.get("#dato-a-eliminar-Ingresos").type(12);
+        cy.get("#form-eliminar-Ingresos").within(() => {
+            cy.get("button").click();
+        });
+        cy.contains("Ingrese aqui para ver los Datos").click();
+        cy.url().should("include", "/MostrarDatos/mostrar.html");
+      });
+
+      it("Verifica que se pueda hacer insertar y eliminar los valores de 12,23,43", () => {
+            cy.visit("/paginas/IngresoDatosDiarios/PaginaPrincipal.html");
+            cy.get('#dato-Ingresos').type(12)
+            cy.get('#form-datos-Ingresos').within(() => {
+                cy.get('button').click();
+            });
+            cy.get('#dato-Ingresos').clear()
+            
+            cy.get('#dato-Ingresos').type(23)
+            cy.get('#form-datos-Ingresos').within(() => {
+                cy.get('button').click();
+            });
+            cy.get('#dato-Ingresos').clear()
+            
+            cy.get('#dato-Ingresos').type(43)
+            cy.get('#form-datos-Ingresos').within(() => {
+                cy.get('button').click();
+            });
+            cy.get('#dato-Ingresos').clear()
+            
+            
+            cy.get("#dato-a-eliminar-Ingresos").type(12);
+            cy.get("#form-eliminar-Ingresos").within(() => {
+                cy.get("button").click();
+            });
+            cy.get("#dato-a-eliminar-Ingresos").clear();
+            
+            cy.get("#dato-a-eliminar-Ingresos").type(23);
+            cy.get("#form-eliminar-Ingresos").within(() => {
+                cy.get("button").click();
+            });
+            cy.get("#dato-a-eliminar-Ingresos").clear();
+            
+            cy.get("#dato-a-eliminar-Ingresos").type(43);
+            cy.get("#form-eliminar-Ingresos").within(() => {
+                cy.get("button").click();
+            });
+            cy.get("#dato-a-eliminar-Ingresos").clear();
+            
+
+            cy.contains("Ingrese aqui para ver los Datos").click();
+            cy.url().should("include", "/MostrarDatos/mostrar.html");
+        });
+});
